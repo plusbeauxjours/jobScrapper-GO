85a68adcbbd63e6e78581c35fafddc042d9cc05f
🫒plusbeauxjours <plusbeauxjours@gmail.com>
2022-03-09 18:46:02 +0700

    set: channels

--- main.go
+++ main.go
@@ -24,10 +24,15 @@ var baseURL string = "https://kr.indeed.com/jobs?q=python&limit=50"
 
 func main() {
 	var jobs []extractedJob
+	c := make(chan []extractedJob)
 	totalPages := getPages()
 
 	for i := 0; i < totalPages; i++ {
-		extractedJobs := getPage(i)
+		go getPage(i, c)
+	}
+
+	for i := 0; i < totalPages; i++ {
+		extractedJobs := <-c
 		jobs = append(jobs, extractedJobs...)
 	}
 	
@@ -55,7 +60,7 @@ func writeJobs(jobs []extractedJob) {
 	}
 }
 
-func getPage(page int) []extractedJob{
+func getPage(page int, mainC chan<- []extractedJob) {
 	var jobs []extractedJob
 	c := make(chan extractedJob)
 	pageURL := baseURL + "&start=" + strconv.Itoa(page*50)
@@ -88,8 +93,9 @@ func getPage(page int) []extractedJob{
 		jobs = append(jobs, job)
 	}
 	
-	return jobs
+	mainC <- jobs
 }
+
 func extractJob(card *goquery.Selection, c chan<- extractedJob) {
 	id, _ := card.Attr("data-jk")
 	title := card.Find("h2>span").Text()
