3795bb054773f9554aad9f377dc3ca293cfdc98c
🫒plusbeauxjours <plusbeauxjours@gmail.com>
2022-03-10 21:58:32 +0700

    set: comment

--- main.go
+++ main.go
@@ -5,3 +5,9 @@ import "github.com/plusbeauxjours/jobScrapper-GO/scrapper"
 func main() {
 	scrapper.Scrape("term")
 }
+
+// go env -w GO111MODULE=on
+// go get github.com/labstack/echo/v4
+
+// go env -w GO111MODULE=on
+// go get github.com/PuerkitoBio/goquery
